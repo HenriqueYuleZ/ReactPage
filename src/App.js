157334bf392d3b4b23e7b2c4cfc148ab83d44094
Henrique Yule <yzhenrique@gmail.com>
2023-06-07 00:03:17 -0400

    Adicionando estilos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { Footer, Blog, Possibility, Features, WhatUnity, Header } from './containers';
 import { NavBar, Brand, CTA } from './components';
 
+import './App.css';
+
 const App = () => {
   return (
     <div className='App'>
